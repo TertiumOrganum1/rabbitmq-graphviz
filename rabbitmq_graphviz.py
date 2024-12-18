47d911b7afbc7224514185abc597f9b63c7cb8db
Владислав Муранов <v.muranov@sirena2000.ru>
2024-12-18 23:30:43 +0300

    fixed is_same_vhost with default value: current rabbitmq topology in json may not have vhost field

--- rabbitmq_graphviz.py
+++ rabbitmq_graphviz.py
@@ -2,13 +2,15 @@ import argparse
 import json
 import sys
 
+
 def escape_id(id_str):
     return id_str.replace('-', '').replace('.', '_')
+
 
 def build_definitions(definitions, vhost, render_producers, render_consumers):
 
     def is_same_vhost(entity_dict):
-        return entity_dict['vhost'] == vhost
+        return entity_dict.get('vhost', '/') == vhost
 
     return ''.join([
         'digraph {\n',
@@ -16,16 +18,21 @@ def build_definitions(definitions, vhost, render_producers, render_consumers):
         '  truecolor=true;\n',
         '  rankdir=LR;\n',
         '  node [style="filled"];\n\n',
-        ''.join([build_queue(q, render_consumers) for q in filter(is_same_vhost, definitions['queues'])]),
-        ''.join([build_exchange(x, render_producers) for x in filter(is_same_vhost, definitions['exchanges'])]),
-        ''.join([build_binding(b) for b in filter(is_same_vhost, definitions['bindings'])]),
-        ''.join([build_dlx_link(q) for q in filter(is_same_vhost, definitions['queues'])]),
+        ''.join([build_queue(q, render_consumers)
+                for q in filter(is_same_vhost, definitions['queues'])]),
+        ''.join([build_exchange(x, render_producers)
+                for x in filter(is_same_vhost, definitions['exchanges'])]),
+        ''.join([build_binding(b)
+                for b in filter(is_same_vhost, definitions['bindings'])]),
+        ''.join([build_dlx_link(q)
+                for q in filter(is_same_vhost, definitions['queues'])]),
         '}'])
+
 
 def build_queue(queue, render_consumers):
     q_name = queue['name']
     q_id = escape_id(q_name)
-    
+
     lines = [
         '  subgraph cluster_Q_%s {\n' % q_id,
         '    label="%s";\n' % q_name,
@@ -35,14 +42,15 @@ def build_queue(queue, render_consumers):
 
     if render_consumers:
         lines.append('  "C_%s" [label="C", fillcolor="#33ccff"];' % q_id)
-        lines.append('  "Q_%(name)s" -> "C_%(name)s"' % { 'name': q_id})
+        lines.append('  "Q_%(name)s" -> "C_%(name)s"' % {'name': q_id})
 
     return ''.join(lines)
+
 
 def build_exchange(exchange, render_producers):
     x_name = exchange['name']
     x_id = escape_id(x_name)
-                
+
     lines = [
         '  subgraph cluster_X_%s {\n' % x_id,
         '    label="%s\\ntype=%s";\n' % (x_name, exchange['type']),
@@ -51,10 +59,12 @@ def build_exchange(exchange, render_producers):
         '  }\n\n']
 
     if render_producers:
-        lines.append('  "P_%s" [label="P", style="filled", fillcolor="#00ffff"];' % x_id)
-        lines.append('  "P_%(name)s" -> "X_%(name)s";' % { 'name': x_id })
+        lines.append(
+            '  "P_%s" [label="P", style="filled", fillcolor="#00ffff"];' % x_id)
+        lines.append('  "P_%(name)s" -> "X_%(name)s";' % {'name': x_id})
 
     return ''.join(lines)
+
 
 def build_binding(binding):
     if binding['destination_type'] == 'exchange':
@@ -62,34 +72,41 @@ def build_binding(binding):
     else:
         return '  X_%s -> Q_%s [label="%s"];\n' % (escape_id(binding['source']), escape_id(binding['destination']), binding['routing_key'])
 
+
 def build_dlx_link(queue):
     q_args = queue.get('arguments', None)
-    if(q_args):
+    if (q_args):
         dlx_name = q_args.get('x-dead-letter-exchange', None)
         msg_ttl = q_args.get('x-message-ttl', None)
-        if(dlx_name):
+        if (dlx_name):
             ttl_txt = ''
-            if(msg_ttl):
+            if (msg_ttl):
                 ttl_txt = '\nMessage TTL: %sms' % msg_ttl
             return '  Q_%s -> X_%s [label="DLX%s", style="dotted"];\n' % (escape_id(queue['name']), escape_id(dlx_name), ttl_txt)
     return ''
 
-def parse_args(): 
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--definitions', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
-                        help='Definitions file')    
+                        help='Definitions file')
     parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
                         help='Output file')
-    parser.add_argument('-p', '--producers', action='store_true', help='Render producers')
-    parser.add_argument('-c', '--consumers', action='store_true', help='Render consumers')    
-    parser.add_argument('-x', '--vhost', nargs='?', default='/', help='Restrict to this vhost')  
-    return parser.parse_args()    
+    parser.add_argument('-p', '--producers',
+                        action='store_true', help='Render producers')
+    parser.add_argument('-c', '--consumers',
+                        action='store_true', help='Render consumers')
+    parser.add_argument('-x', '--vhost', nargs='?',
+                        default='/', help='Restrict to this vhost')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
 
     definitions = json.load(args.definitions)
     args.definitions.close()
-        
-    args.outfile.write(build_definitions(definitions, args.vhost, args.producers, args.consumers))
+
+    args.outfile.write(build_definitions(
+        definitions, args.vhost, args.producers, args.consumers))
     args.outfile.close()
